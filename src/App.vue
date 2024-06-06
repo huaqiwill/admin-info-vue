c57f6ce41d9da8ebb366030bde24e2be8457a276
huaqiwill <3173484026@qq.com>
2024-06-07 00:57:38 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="overflow-y: hidden; overflow-x: hidden">
+    <router-view />
+  </div>
+</template>
+
+<style></style>
+<script>
+export default {
+  name: "App",
+};
+</script>
+
